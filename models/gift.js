99bb7987acf2427b2ba996ae591ff2c0ee3101e8
JasenABaker <JasenABaker@gmail.com>
2017-12-31 17:40:02 -0500

    Added gift and store models

---
+++ models/gift.js
@@ -0,0 +1,6 @@
+const Schema = require('../db/schema')
+const mongoose = require('mongoose')
+
+const Gift = mongoose.model('Gift', Schema.GiftSchema)
+
+module.exports = Gift
---
+++ models/store.js
@@ -0,0 +1,6 @@
+const Schema = require('../db/schema')
+const mongoose = require('mongoose')
+
+const Store = mongoose.model('Store', Schema.StoreSchema)
+
+module.exports = Store
